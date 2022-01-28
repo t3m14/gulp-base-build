463aaa243e42b14fadb18dfa4795ff43766d5534
t3m14 <artemon.m174@gmail.com>
2022-01-28 18:14:58 +0500

    base build

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,38 @@
-const { src, dest, watch, parallel } = require('gulp');
+const { src, dest, watch, parallel, series } = require('gulp');
 const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat')
 const browserSync = require('browser-sync').create();
 const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin')
+const del = require('del')
+
+function build() {
+    return src([
+        'app/css/style.min.css',
+         'app/fonts/**/*',
+         'app/js/main.min.js',
+         'app/*.html'
+    ], {base: 'app'})
+    .pipe(dest('dist'))
+
+}
 
 function browsersync() {
     browserSync.init({
         server: {
             baseDir: 'app/'
         }
-    })
+    });
+}
+
+function clearDist (){
+    return del('dist')
+}
+function images() {
+    return src('app/images/**/*')
+    .pipe(imagemin())
+    .pipe(dest('dist/images'))
 }
 
 function scripts() {
@@ -23,10 +46,14 @@ function scripts() {
     .pipe(browserSync.stream())
 
 }
+
 function styles() {
     return src('app/scss/style.scss')
         .pipe(scss({ outputStyle: 'compressed' }))
         .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            overrideBrowserslist : ['last 10 versions']
+        }))
         .pipe(dest('app/css'))
         .pipe(browserSync.stream())
 }
@@ -40,5 +67,9 @@ function watching() {
 exports.styles = styles;
 exports.watching = watching;
 exports.browsersync = browsersync;
-exports.scripts = scripts
-exports.default = parallel(scripts, browsersync, watching);
+exports.scripts = scripts;
+exports.images = images;
+exports.cleanDist = clearDist;
+
+exports.build = series(clearDist, images, build);
+exports.default = parallel(styles, scripts, browsersync, watching)
